e94f0d5b80e9823c3a6b91336128291f75da2a24
Lucas Barga <lucasmpbarga@gmail.com>
2018-12-07 00:50:17 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import Header from './src/components/header'
+
+const App = () => (
+    <Header headerText = {'Albums'}/>
+);
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    ); 
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        borderColor: '#000000',
+        borderBottomWidth: 3
+    },
+    textStyle: {
+        fontSize: 25
+    }
+};
+
+export default Header;
